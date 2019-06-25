f5a292b2f9c1689a007549fdb346424372fa46a1
Conor Hughes <conorhughes93@gmail.com>
2019-06-25 22:50:08 +0100

    Update/Remove playlist

--- server/client/src/actions/types.js
+++ server/client/src/actions/types.js
@@ -1,4 +1,6 @@
 export const FETCH_USER = 'fetch_user';
 export const FETCH_PLAYLISTS = 'fetch_playlists';
-export const SUBMIT_PLAYLIST = 'submit_playlist';
+export const CREATE_PLAYLIST = 'create_playlist';
+export const UPDATE_PLAYLIST = 'update_playlist';
+export const REMOVE_PLAYLIST = 'remove_playlist';
 export const FETCH_SPOTIFY_PLAYLISTS = 'fetch_spotify_playlists';
--- server/client/src/components/Playlist.js
+++ server/client/src/components/Playlist.js
@@ -4,25 +4,38 @@ import Media from 'react-bootstrap/Media';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
 
-export default function Playlist({ image, title, tracks }) {
+export default function Playlist({ id, image, title, cacheSize, showPlaylist, removePlaylist }) {
   return (
-    <ListGroup.Item>
+    <ListGroup.Item action onClick={() => showPlaylist(title, id, cacheSize)}>
       <Media>
-        <Image src={image} style={{ height: '70px', width: '70px' }} rounded />
+        <Image src={image} style={{ height: '52px', width: '52px' }} rounded />
         <Media.Body className="ml-4">
-          <h5>{title}</h5>
-          <p>
-            {tracks}
+          <h5 className="mb-1">{title}</h5>
+          <p className="mb-0">
+            {cacheSize}
             &nbsp;tracks
           </p>
         </Media.Body>
+        <button
+          type="submit"
+          className="close"
+          onClick={e => {
+            removePlaylist(e, id);
+          }}
+        >
+          <span aria-hidden="true">×</span>
+          <span className="sr-only">Delete</span>
+        </button>
       </Media>
     </ListGroup.Item>
   );
 }
 
 Playlist.propTypes = {
+  id: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  tracks: PropTypes.number.isRequired
+  cacheSize: PropTypes.number.isRequired,
+  showPlaylist: PropTypes.func.isRequired,
+  removePlaylist: PropTypes.func.isRequired
 };
